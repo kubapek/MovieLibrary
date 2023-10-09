3345673e911ffd17a8a24b9c821f2730b9ec5634
kuba <kuba200213@gmail.com>
2023-10-09 14:09:52 +0200

    added postgres and docker-compose

--- src/main/java/com/movielibrary/MovieLibraryApplication.java
+++ src/main/java/com/movielibrary/MovieLibraryApplication.java
@@ -2,8 +2,10 @@ package com.movielibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class MovieLibraryApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/movielibrary/movie/domain/repository/MovieRepository.java
+++ src/main/java/com/movielibrary/movie/domain/repository/MovieRepository.java
@@ -2,9 +2,11 @@ package com.movielibrary.movie.domain.repository;
 
 import com.movielibrary.movie.domain.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Query("UPDATE Movie m SET m.title = :#{#newMovie.title}, m.director = :#{#newMovie.director} WHERE m.id = :id")
     Movie updateById(Long id, Movie newMovie);
 }
