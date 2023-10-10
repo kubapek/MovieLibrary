368737a1de14b8f36fb59bc514a65517b864dd90
kuba <kuba200213@gmail.com>
2023-10-10 15:15:45 +0200

    added schema, sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,50 @@
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The Shawshank Redemption', 'Frank Darabont');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The Godfather', 'Francis Ford Coppola');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The Dark Knight', 'Christopher Nolan');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The Godfather Part II', 'Francis Ford Coppola');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), '12 Angry Men', 'Sidney Lumet');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Schindler''s List', 'Steven Spielberg');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The  Lord of The Ring: The Return of The King', 'Peter Jackson');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Pulp Fiction', 'Quentin Tarantino');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The  Lord of The Ring: The Fellowship of The Ring', 'Peter Jackson');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The Good, the Bad and the Ugly', 'Sergio Leone');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Forrest Gump', 'Robert Zemeckis');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), '***** ****', 'David Fincher');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The  Lord of The Ring: The Two Towers', 'Peter Jackson');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Inception', 'Christopher Nolan');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Star Wars: Episode V - The Empire Strikes Back', 'Irvin Kershner');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The Matrix', 'Lana Wachowski');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Goodfellas', 'Martin Scorsese');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'One Flew Over The Cuckoo''s Nest', 'Milos Forman');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Se7en', 'David Fincher');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Spider-Man: Across the Spider-Verse', 'Joaquim Dos Santos');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'It''s a Wonderful Life', 'Frank Capra');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Seven Samurai', 'Akira Kurosawa');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Interstellar', 'Christopher Nolan');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'The Silence of the Lambs', 'Jonathan Demme');
+INSERT INTO movie (id, title, director)
+VALUES (nextval('movie_id_seq'), 'Saving Private Ryan', 'Steven Spielberg');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS movie
+(
+    id     BIGSERIAL    PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    director   VARCHAR(255) NOT NULL
+);
